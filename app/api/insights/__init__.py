8336e4804f0206ae25906c4babea818675e912ff
Joaquín Bernal <jobedom@gmail.com>
2024-03-19 04:08:57 +0100

    Add new insight for positive samples count

--- app/api/insights/__init__.py
+++ app/api/insights/__init__.py
@@ -1,7 +1,9 @@
+from .insight_positive_count import calculate as calculate_positive_count_insight
 from .insight_zero_crossings import calculate as calculate_zero_crossings_insight
 
 insight_calculators = [
     calculate_zero_crossings_insight,
+    calculate_positive_count_insight,
 ]
 
 
--- app/api/insights/schema.py
+++ app/api/insights/schema.py
@@ -3,9 +3,10 @@ from typing import Union
 from pydantic import BaseModel
 from typing_extensions import TypedDict
 
+from .insight_positive_count import PositiveCountInsightSchema
 from .insight_zero_crossings import ZeroCrossingsInsightSchema
 
-AnyInsightSchema = Union[ZeroCrossingsInsightSchema]
+AnyInsightSchema = Union[ZeroCrossingsInsightSchema, PositiveCountInsightSchema]
 
 
 class LeadInsights(TypedDict):

0cdf88518d79b164193dc11d7aa819ab8c288803
Joaquín Bernal <jobedom@gmail.com>
2024-03-19 04:14:01 +0100

    Add missing typing

--- app/api/insights/__init__.py
+++ app/api/insights/__init__.py
@@ -1,9 +1,12 @@
+from app.models.lead import Lead
+
 from .insight_zero_crossings import calculate as calculate_zero_crossings_insight
+from .schema import AnyInsightSchema
 
 insight_calculators = [
     calculate_zero_crossings_insight,
 ]
 
 
-def calculate_insights(lead):
+def calculate_insights(lead: Lead) -> list[AnyInsightSchema]:
     return [calculator(lead) for calculator in insight_calculators]

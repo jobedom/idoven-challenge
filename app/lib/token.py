1db9b985718b24e541fe6e9a66f2c13b8d986bc6
Joaquín Bernal <jobedom@gmail.com>
2024-03-20 01:55:08 +0100

    Add auxiliary modules for password and token handling

---
+++ app/lib/token.py
@@ -0,0 +1,27 @@
+from datetime import datetime, timedelta
+from typing import Any, Union
+
+from jose import jwt
+
+from app.settings import settings
+
+
+def _create_token(
+    subject: Union[str, Any], token_expire_minutes: int, secret_key: str, expires_delta: int = None
+) -> str:
+    if expires_delta is not None:
+        expires_delta = datetime.now(datetime.UTC) + expires_delta
+    else:
+        expires_delta = datetime.now(datetime.UTC) + timedelta(minutes=token_expire_minutes)
+
+    to_encode = {"exp": expires_delta, "sub": str(subject)}
+    encoded_jwt = jwt.encode(to_encode, secret_key, settings.TOKEN_ALGORITHM)
+    return encoded_jwt
+
+
+def create_access_token(subject: Union[str, Any], expires_delta: int = None) -> str:
+    return _create_token(subject, settings.ACCESS_TOKEN_EXPIRE_MINUTES, settings.JWT_SECRET_KEY, expires_delta)
+
+
+def create_refresh_token(subject: Union[str, Any], expires_delta: int = None) -> str:
+    return _create_token(subject, settings.REFRESH_TOKEN_EXPIRE_MINUTES, settings.JWT_REFRESH_SECRET_KEY, expires_delta)

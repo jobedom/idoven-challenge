ce259d4e9e3a85b6d57e713e45cadf29bfb0ba48
Joaquín Bernal <jobedom@gmail.com>
2024-03-20 01:54:05 +0100

    Add token settings to module

--- app/settings.py
+++ app/settings.py
@@ -3,10 +3,21 @@ from pathlib import Path
 
 from pydantic_settings import BaseSettings, SettingsConfigDict
 
+FIXED_SETTINGS = {
+    "ACCESS_TOKEN_EXPIRE_MINUTES": 30,  # 30 minutes
+    "REFRESH_TOKEN_EXPIRE_MINUTES": 60 * 24 * 7,  # 7 days
+    "TOKEN_ALGORITHM": "HS256",
+}
+
 
 class Settings(BaseSettings):
     DB_URI: str
     ECHO_SQL: bool
+    ACCESS_TOKEN_EXPIRE_MINUTES: int
+    REFRESH_TOKEN_EXPIRE_MINUTES: int
+    TOKEN_ALGORITHM: str
+    JWT_SECRET_KEY: str
+    JWT_REFRESH_SECRET_KEY: str
 
     model_config = SettingsConfigDict(
         env_file=Path(__file__).parent / f"config/{os.environ['APP_CONFIG_FILE']}.env",
@@ -14,4 +25,4 @@ class Settings(BaseSettings):
     )
 
 
-settings = Settings.model_validate({})
+settings = Settings.model_validate(FIXED_SETTINGS)
